4b7d911c192645dc1b8c92223cc4a41b26393fb3
Aarushi Ramesh <aarushiram1@gmail.com>
2020-08-29 22:23:45 -0700

    Update light_detection_system.ino

--- Light_Detection_System/light_detection_system.ino
+++ Light_Detection_System/light_detection_system.ino
@@ -1,26 +1,26 @@
 //Using a photoresistor to light an LED
 //Photoresistor Light Alarm Detection System!!
 
-const int photoResistor = A0; // Photoresistor connected at analog pin A1
-const int ledPin=8;       // LED connected connected at Arduino pin 9
+const int photoResistor = A0;    // Photoresistor connected at analog pin A1
+const int ledPin=8;              // LED connected connected at Arduino pin 9
 
 //Variables
-int value;				        // Store value from photoresistor (between a certain range of values)
+int value;				                   // Store value from photoresistor (between a certain range of values)
 
 void setup(){
- pinMode(ledPin, OUTPUT);       // Setting lepPin - 8 pin as an OUTPUT PIN
- pinMode(photoResistor, INPUT);     // Setting pResistor - A1 pin as an INPUT PIN
+ pinMode(ledPin, OUTPUT);        // Setting lepPin - 8 pin as an OUTPUT PIN
+ pinMode(photoResistor, INPUT);  // Setting pResistor - A1 pin as an INPUT PIN
 }
 
 void loop(){
   value = analogRead(photoResistor);
   //threshold value is at a 50
   if (value > 800){
-    digitalWrite(ledPin, LOW);  //LED OFF
+    digitalWrite(ledPin, LOW);   //LED OFF
   }
   else{
-    digitalWrite(ledPin, HIGH); //LED ON
+    digitalWrite(ledPin, HIGH);  //LED ON
   }
 
-  delay(400); //DELAYING IT
+  delay(400);                    //DELAYING IT
 }
